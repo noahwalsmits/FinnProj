806094bef7f92e233dbb70c05c38bb7ad4caf0a2
Noah <nwalsmits@outlook.com>
2021-02-17 16:49:11 +0100

    Imrpoved flappycharacter

--- src/sample/game/FlappyCharacter.java
+++ src/sample/game/FlappyCharacter.java
@@ -6,36 +6,54 @@ import sample.game.drawable.ScreenConfig;
 
 public class FlappyCharacter {
     private DrawableImage drawable;
-    private AudioClip jumpSound;
-    private double height;
-    private double yVelocity;
+    private final AudioClip jumpSound;
+    private final AudioClip bumpSound;
+    private double height; //height based on base screen height
+    private double yVelocity; //the height is changed by the velocity every second
+
+    private static final double GRAVITY = 300.0;
+    private static final double JUMP_BASE_VELOCITY = 100.0;
+    private static final double JUMP_ADDED_VELOCITY = 150.0;
+    private static final double SPRITE_HEIGHT = 150.0;
 
     public FlappyCharacter() {
-        this.drawable = new DrawableImage(50, 50, 300, 300, "penguin.png");
+        this.drawable = new DrawableImage(50, 50, SPRITE_HEIGHT, SPRITE_HEIGHT, "penguin.png");
         this.jumpSound = new AudioClip(getClass().getResource("/sounds/nsmbwiiJump1.wav").toString());
-        this.height = 0.0;
+        this.bumpSound = new AudioClip(getClass().getResource("/sounds/nsmbwiiBump.wav").toString());
+        this.height = 500.0;
         this.yVelocity = 0.0;
     }
 
     public void update(double elapsedTime) {
-//        this.drawable.setBaseY(ScreenConfig.BASE_SCREEN_HEIGHT - this.height);
-        if (this.drawable.getBaseY() < ScreenConfig.BASE_SCREEN_HEIGHT) {
-            this.yVelocity += 300.0 * elapsedTime;
+        if (this.height > SPRITE_HEIGHT) { //when character not touching ground
+            this.yVelocity -= GRAVITY * elapsedTime; //apply gravity to velocity
+            if (this.height > ScreenConfig.BASE_SCREEN_HEIGHT) { //when character touches roof
+                this.height = ScreenConfig.BASE_SCREEN_HEIGHT;
+                this.yVelocity *= -1.0;
+                this.bumpSound.play();
+            }
+        } else { //when character is touching ground
+            this.gameOver();
         }
-        this.drawable.setBaseY(this.drawable.getBaseY() + this.yVelocity * elapsedTime);
+        this.height += this.yVelocity * elapsedTime; //change height by velocity
 
-        if (this.drawable.getRotation() < 0.0) {
-            this.drawable.setRotation(this.drawable.getRotation() + 100.0 * elapsedTime);
+        if (this.drawable.getRotation() < 0.0) { //when rotation not reset
+            this.drawable.setRotation(this.drawable.getRotation() + ((-this.drawable.getRotation() * 0.5 + 100.0) * elapsedTime)); //gradually reset rotation
         }
+        this.drawable.setBaseY(ScreenConfig.BASE_SCREEN_HEIGHT - this.height); //change sprite height
     }
 
     public void jump() {
-        if (this.yVelocity > -100.0) {
-            this.yVelocity = -100.0;
+        if (this.yVelocity < JUMP_BASE_VELOCITY) {
+            this.yVelocity = JUMP_BASE_VELOCITY;
         }
-        this.yVelocity += -150.0;
+        this.yVelocity += JUMP_ADDED_VELOCITY;
         this.jumpSound.play();
         this.drawable.setRotation(this.drawable.getRotation() - 30.0);
+    }
+
+    public void gameOver() {
+        //todo
     }
 
     public DrawableImage getDrawable() {
