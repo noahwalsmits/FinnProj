937a30aed485573dde4e5273410ef5b1c5908d08
Noah <nwalsmits@outlook.com>
2021-03-07 20:55:36 +0100

    Added placeholder social screen

--- src/sample/social/SocialScreen.java
+++ src/sample/social/SocialScreen.java
@@ -1,17 +1,27 @@
 package sample.social;
 
 import javafx.scene.control.Button;
-import javafx.scene.layout.FlowPane;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import sample.ContentScreen;
 
 public class SocialScreen implements ContentScreen {
     private final Pane root;
 
     public SocialScreen() {
-        this.root = new FlowPane();
-        this.root.getChildren().add(new Button(getClass().getProtectionDomain().getCodeSource().getLocation().toString().substring(6)+ "/../userdata.json"));
-        //TODO placeholder screen
+        this.root = new VBox();
+
+
+        HBox serverBox = new HBox();
+        serverBox.getChildren().add(new Button("view servers"));
+        serverBox.getChildren().add(new Button("create server"));
+        this.root.getChildren().add(serverBox);
+
+        this.root.getChildren().add(new Label("Players found:"));
+        this.root.getChildren().add(new Label("The social screen has not yet been implemented. The purpose " +
+                "of this was to let players trade items over a LAN connection."));
     }
 
     @Override
